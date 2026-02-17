02ee71d22dd093f83628a3b1ead4d6eac3c9c258
Toshihiro Morimoto <m@dealfore.st>
2026-02-17 22:47:23 +0900

    feat(tmux): choose-tree のプレビューを非表示にする

--- tmux/scripts/choose-tree-filtered.sh
+++ tmux/scripts/choose-tree-filtered.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PANE=$(tmux show-option -gv @ct_pane)
-tmux choose-tree -t "$PANE" -sf '#{==:#{session_name},#{@ct_session}}' \
+tmux choose-tree -t "$PANE" -NNsf '#{==:#{session_name},#{@ct_session}}' \
   "select-window -t '%%' ; run-shell '$HOME/.config/tmux/scripts/choose-tree-filtered.sh'"
 tmux send-keys -t "$PANE" Right
